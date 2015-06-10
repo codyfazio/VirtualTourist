2732dc26a1d47dace27be7b2f9ef2d36b2c117e1
codyfazio <codyfazio@icloud.com>
2015-06-09 18:17:15 -0600

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Cody Clingan on 6/9/15.
+//  Copyright (c) 2015 Cody Fazio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
